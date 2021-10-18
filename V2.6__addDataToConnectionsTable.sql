619030b500e86ccca5ce13f694ade1ce4f6906f7
AnastasiaChukhina <nas2002@list.ru>
2021-10-19 01:27:29 +0300

    Connections added

---
+++ V2.6__addDataToConnectionsTable.sql
@@ -0,0 +1,28 @@
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Ivanov Mikhail Yurievich'),
+                                                                 (select personnel_number from worker where name = 'Gusev Boris Danilovich')
+                                                                 );
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Ivanov Mikhail Yurievich'),
+                                                                 (select personnel_number from worker where name = 'Gavrilov Mitrofan Petrovich')
+                                                                 );
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Ivanov Mikhail Yurievich'),
+                                                                 (select personnel_number from worker where name = 'Mironov Askold Proclovich')
+                                                                 );
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Gusev Boris Danilovich'),
+                                                                 (select personnel_number from worker where name = 'Smirnova Elina Filipovna')
+                                                                 );
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Gavrilov Mitrofan Petrovich'),
+                                                                 (select personnel_number from worker where name = 'Mironova Juno Veriaminovna')
+                                                                 );
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Mironov Askold Proclovich'),
+                                                                 (select personnel_number from worker where name = 'Voronova Anna Kuprianova')
+                                                                 );
+insert into workers_employees (worker_num, employee_num) values (
+                                                                (select personnel_number from worker where name ='Mironov Askold Proclovich'),
+                                                                 (select personnel_number from worker where name = 'Potapova Radmila Antoninovna')
+                                                                 );
