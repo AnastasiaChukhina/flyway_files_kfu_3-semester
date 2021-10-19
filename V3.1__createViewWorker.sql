3f9a70c8ed3d042dbf803e3344d0a259a0cc2c2e
AnastasiaChukhina <nas2002@list.ru>
2021-10-19 10:01:30 +0300

    Views created

---
+++ V3.1__createViewWorker.sql
@@ -0,0 +1,3 @@
+create or replace view v_worker as
+            select *
+            from worker;
---
+++ V3.2__createViewConnections.sql
@@ -0,0 +1,3 @@
+create or replace view v_workers_employees as
+            select *
+            from workers_employees;
